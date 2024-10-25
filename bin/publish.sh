9855100e230730dd9c8fcf676d9503a3520a0c29
Panos Sakkos <panos.sakkos@gmail.com>
2024-10-25 14:26:00 +0300

    update publish script

--- bin/publish.sh
+++ bin/publish.sh
@@ -7,7 +7,7 @@ echo "Generating posts..."
 ruby bin/generate_tags.rb
 
 echo "Building site..."
-JEKYLL_ENV=production jekyll build
+JEKYLL_ENV=production bundle exec jekyll build
 
 echo "Publishing..."
 rsync --delete --progress --stats -ru _site/* -e "ssh -i ~/key.pem" ec2-user@ec2-18-196-68-187.eu-central-1.compute.amazonaws.com:/usr/share/nginx/html
