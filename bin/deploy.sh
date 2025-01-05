3d41cf87e87b095de24c6104825a20167e07c656
Panos Sakkos <panos.sakkos@gmail.com>
2025-01-05 21:49:50 +0200

    update deployment script

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -10,4 +10,4 @@ echo "Building site..."
 JEKYLL_ENV=production bundle exec jekyll build
 
 echo "Publishing..."
-rsync --delete --progress --stats -ru _site/* -e "ssh -i ~/key.pem" ec2-user@ec2-18-196-68-187.eu-central-1.compute.amazonaws.com:/usr/share/nginx/html
+rsync --delete --progress --stats -ru _site/* -e "ssh -i ~/memoryleaks.pem" ec2-user@ec2-18-196-68-187.eu-central-1.compute.amazonaws.com:/usr/share/nginx/html
